ff5339bc401f6128b6d4be5ec1d2b1ddec83f9b6
Rishabhkr1802 <krrishabh1802@gmail.com>
2025-10-16 22:56:11 +0530

    Update for Deployment

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -2,6 +2,7 @@ import dotenv       from "dotenv";
 import express      from "express";
 import cookieparser from "cookie-parser";
 import cors         from "cors";
+import path         from "path";        // For Deployment
 
 import authRouter   from "./router/auth.routes.js";
 import chatRouter   from "./router/chat.routes.js";
@@ -14,6 +15,8 @@ dotenv.config({
 
 const port = process.env.PORT || 3000;
 const app  = express();
+
+const __dirname = path.resolve();       // For Deployment
 
 app.use(cors({
     origin: ['http://localhost:3000','http://192.168.0.101:3000'],
@@ -28,6 +31,14 @@ app.use(cookieparser());
 app.use('/api/v1/auth',  authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/chat',  chatRouter);
+
+// For Deployment
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../../Frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(port, () => {
     console.log(`Server is running at ${port}`);
