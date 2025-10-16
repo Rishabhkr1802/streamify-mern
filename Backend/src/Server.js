2c649772ab03451d1cd65f12e12dee23d9596cb2
Rishabhkr1802 <krrishabh1802@gmail.com>
2025-10-17 00:19:46 +0530

    Update Expressv5 syntax

--- Backend/src/Server.js
+++ Backend/src/Server.js
@@ -35,7 +35,7 @@ app.use('/api/v1/chat',  chatRouter);
 // For Deployment
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../../Frontend/dist")));
-    app.get("*", (req, res) => {
+    app.get("/*splat", (req, res) => {
         res.sendFile(path.join(__dirname, "../../Frontend", "dist", "index.html"));
     })
 }
